dc15f98391ea7c2450b4f81117f2204ad1cddf4b
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-09-26 10:42:21 +0200

    zz

--- app/database.py
+++ app/database.py
@@ -2,18 +2,13 @@ import mysql.connector
 from mysql.connector import Error
 
 mydb = {
-    'host': '10.100.100.210',
+    'host': '127.0.0.1',
     'user': 'pokedata',
     'password': 'pokekopi',
     'database': 'pokedex'
 }
 
-mydbLh = {
-    'host': '127.0.0.1',
-    'user': 'root',
-    'password': 'Korn123',
-    'database': 'pokedex'
-}
+
 
 
 # Funksjon for å opprette en tilkobling til databasen
